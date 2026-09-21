d211936720a8b502f720e8f40697e44aca06404f
bryanwaine <nwanne.ezeaka@gmail.com>
2026-09-21 13:56:54 +0100

    deploy.yml:update deployment block to check for repo on instance first and clone if absent

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = require("./app");
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
