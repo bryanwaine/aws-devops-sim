1a49a34c6a9975aa42f9c147da9920a2218d9865
bryanwaine <nwanne.ezeaka@gmail.com>
2026-09-23 11:59:48 +0100

    app.test.js: edit test suite to fail

--- app.test.js
+++ app.test.js
@@ -5,7 +5,7 @@ describe("Get /health status", () => {
   it("should return status code 200 and status OK", async () => {
     const response = await request(app).get("/health");
 
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({ status: "OK" });
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual({ status: "Not OK" });
   });
 });
